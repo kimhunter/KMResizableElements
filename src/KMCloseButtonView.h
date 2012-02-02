febc2c1b44a4fa38d16cbc717abea834a6f88e69
Kim Hunter <bigkm1@gmail.com>
2012-02-02 23:04:20 +1000

    added close button Added licence and kitspec

---
+++ src/KMCloseButtonView.h
@@ -0,0 +1,16 @@
+//
+//  KMCloseButtonView.h
+//  Elements
+//
+//  Created by Kim Hunter on 2/02/12.
+//  Copyright (c) 2012 Kim Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMCloseButtonView : UIView
+@property (nonatomic, retain) UIColor *outerCircleColor;
+@property (nonatomic, retain) UIColor *innerCircleColor;
+@property (nonatomic, retain) UIColor *crossColor;
+@property (nonatomic, assign) BOOL drawShadow;
+@end
---
+++ src/KMCloseButtonView.m
@@ -0,0 +1,121 @@
+//
+//  KMCloseButtonView.m
+//  Elements
+//
+//  Created by Kim Hunter on 2/02/12.
+//  Copyright (c) 2012 Kim Hunter. All rights reserved.
+//
+
+#import "KMCloseButtonView.h"
+
+
+@interface KMCloseButtonView ()
+- (void)defaultInitialisation;
+@end
+
+
+
+@implementation KMCloseButtonView
+
+@synthesize outerCircleColor;
+@synthesize innerCircleColor;
+@synthesize crossColor;
+@synthesize drawShadow;
+
+- (void)dealloc
+{
+    self.outerCircleColor = nil;
+    self.innerCircleColor = nil;
+    self.crossColor = nil;
+    [super dealloc];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        [self defaultInitialisation];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self)
+    {
+        [self defaultInitialisation];
+    }
+    return self;
+}
+
+- (void)defaultInitialisation
+{
+    self.outerCircleColor = [UIColor whiteColor];
+    self.innerCircleColor = [UIColor blackColor];
+    self.crossColor = outerCircleColor;
+    drawShadow = YES;
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    UIBezierPath *outer, *inner;
+    CGRect r = self.bounds;
+    CGFloat offset = 0.87f;
+    CGRect innerRect;
+    
+    if (drawShadow)
+    {
+        //tweak rect r to allow room for shadow
+        r = CGRectApplyAffineTransform(r, CGAffineTransformMakeScale(offset, offset));
+        r = CGRectIntegral(r);
+    }
+    
+
+    innerRect = CGRectApplyAffineTransform(r, CGAffineTransformMakeScale(offset, offset));
+    innerRect = CGRectIntegral(CGRectOffset(innerRect, (r.size.width-innerRect.size.width)/2, (r.size.height-innerRect.size.height)/2));
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetShouldAntialias(context, YES);
+    
+    // Draw outline circle
+    CGContextSaveGState(context);
+    if (drawShadow)
+    {
+        //use the space between rect r and the bounds for offset and blur
+        CGContextSetShadow(context, CGSizeMake((self.bounds.size.width - r.size.width)/2, (self.bounds.size.height - r.size.height)/2), (self.bounds.size.width - r.size.width)/2);
+    }
+    [outerCircleColor setFill];
+    CGContextFillEllipseInRect(context, r);
+    CGContextRestoreGState(context);
+    
+    
+    [innerCircleColor setFill];
+    CGContextFillEllipseInRect(context, innerRect);
+    
+    // save state to clip the lines
+    CGContextSaveGState(context);
+    // now draw the cross
+    [crossColor setStroke];
+    CGFloat lineWidth = roundf((r.size.width-innerRect.size.width)/2 * 1.3);
+    CGContextSetLineWidth(context, lineWidth);
+    
+    CGRect clipBounds = CGRectInset(innerRect, innerRect.size.width * 0.2, innerRect.size.width * 0.2);
+    CGRectOffset(rect, (r.size.width - innerRect.size.width)/2, (r.size.height - innerRect.size.height)/2);
+    CGContextClipToRect(context, clipBounds);
+    CGContextBeginPath(context);
+    // draw backslash line 
+    CGContextMoveToPoint(context, innerRect.origin.x, innerRect.origin.y);
+    CGContextAddLineToPoint(context, CGRectGetMaxX(innerRect), CGRectGetMaxY(innerRect));
+    // draw fwd slash line /
+    CGContextMoveToPoint(context, innerRect.origin.y, CGRectGetMaxY(innerRect));
+    CGContextAddLineToPoint(context, CGRectGetMaxX(innerRect), innerRect.origin.y);
+    CGContextDrawPath(context, kCGPathStroke);
+    
+    CGContextRestoreGState(context);
+}
+
+@end
