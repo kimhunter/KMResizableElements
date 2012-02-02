a1dad4ceff1065abf6dbc3a0b906aedbfe55a61f
Kim Hunter <bigkm1@gmail.com>
2012-02-02 21:00:12 +1000

    Initial Commit

---
+++ Elements/KMAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  KMAppDelegate.h
+//  Elements
+//
+//  Created by Kim Hunter on 2/02/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class KMViewController;
+
+@interface KMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) KMViewController *viewController;
+
+@end
---
+++ Elements/KMViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMViewController.h
+//  Elements
+//
+//  Created by Kim Hunter on 2/02/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMViewController : UIViewController
+
+@end
