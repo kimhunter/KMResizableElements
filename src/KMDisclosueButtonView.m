66f7fe6aa5e266ba44e672311cee00f0407856aa
Kim Hunter <bigkm1@gmail.com>
2012-02-04 11:13:06 +1000

    wip

--- src/KMDisclosueButtonView.m
+++ src/KMDisclosueButtonView.m
@@ -19,13 +19,26 @@
     return self;
 }
 
-/*
+#define COLOR2PERC(c) ((CGFloat)((c)/255))
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGRect r = self.bounds;
+    CGFloat insetPercentage = 0.1f;
+    CGRect mainRect = CGRectIntegral(CGRectInset(r, r.size.width*insetPercentage, r.size.height*insetPercentage));
+
+    [[UIColor whiteColor] setStroke];
+    UIColor *c = [UIColor colorWithRed:COLOR2PERC(35.f) green:COLOR2PERC(110.0f) blue:COLOR2PERC(216.f) alpha:1.0];
+    [c setFill];
+    CGContextSetLineWidth(context, r.size.width * insetPercentage * 0.8);
+    CGContextAddEllipseInRect(context, mainRect);
+    CGContextDrawPath(context, kCGPathFillStroke);
+    
+    
 }
-*/
+
 
 @end
