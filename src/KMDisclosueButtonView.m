3b3722a66d7a400eda4d30bef027871e1e9131d7
Kim Hunter <bigkm1@gmail.com>
2012-02-04 15:42:19 +1000

    gloss working

--- src/KMDisclosueButtonView.m
+++ src/KMDisclosueButtonView.m
@@ -18,8 +18,22 @@
     }
     return self;
 }
-
+#define COLOR_CoolBlue [UIColor colorWithRed:COLOR2PERC(35.f) green:COLOR2PERC(110.0f) blue:COLOR2PERC(216.f) alpha:1.0]
 #define COLOR2PERC(c) ((CGFloat)((c)/255))
+
+
+
+- (CGGradientRef)glossGradient
+{
+    CGGradientRef glossGradient;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGFloat locs[] = {0.0f, 1.0f};
+    CGColorRef colorRefs[] = { [[UIColor colorWithWhite:1.0 alpha:0.6] CGColor],
+                               [[UIColor colorWithWhite:1.0 alpha:0.3] CGColor]};
+    CFArrayRef colors = CFArrayCreate(NULL, (const void**)colorRefs, sizeof(colorRefs) / sizeof(CGColorRef), &kCFTypeArrayCallBacks);
+    glossGradient = CGGradientCreateWithColors(colorSpace, colors, locs);    
+    return glossGradient;
+}
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
@@ -31,11 +45,27 @@
     CGRect mainRect = CGRectIntegral(CGRectInset(r, r.size.width*insetPercentage, r.size.height*insetPercentage));
 
     [[UIColor whiteColor] setStroke];
-    UIColor *c = [UIColor colorWithRed:COLOR2PERC(35.f) green:COLOR2PERC(110.0f) blue:COLOR2PERC(216.f) alpha:1.0];
-    [c setFill];
-    CGContextSetLineWidth(context, r.size.width * insetPercentage * 0.8);
+    [COLOR_CoolBlue setFill];
+    CGContextSetLineWidth(context, mainRect.size.width * insetPercentage * 0.8);
     CGContextAddEllipseInRect(context, mainRect);
     CGContextDrawPath(context, kCGPathFillStroke);
+
+    CGContextSaveGState(context);
+
+    CGContextBeginPath(context); 
+    CGRect glossRect = CGRectOffset(rect, 0.0f, -60.0);
+    
+    CGContextAddEllipseInRect(context, glossRect);
+    CGContextClip(context);
+    CGContextAddEllipseInRect(context, CGRectInset(mainRect, mainRect.size.width * insetPercentage * -0.8/2, mainRect.size.width * insetPercentage * -0.8/2));
+    CGContextClip(context);
+    CGContextDrawLinearGradient(context, [self glossGradient], glossRect.origin, CGPointMake(glossRect.origin.x, CGRectGetMaxY(glossRect)), kCGGradientDrawsBeforeStartLocation);
+
+    
+
+    CGContextClosePath(context);
+    CGContextRestoreGState(context);
+
     
     
 }
