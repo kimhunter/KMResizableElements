fcd542f13951bae3154d45c2e600a65f7ff49a98
Kim Hunter <bigkm1@gmail.com>
2012-02-04 10:27:09 +1000

    Add disclosure button class

---
+++ src/KMDisclosueButtonView.h
@@ -0,0 +1,13 @@
+//
+//  KMDisclosueButtonView.h
+//  Elements
+//
+//  Created by Kim Hunter on 4/02/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMDisclosueButtonView : UIView
+
+@end
---
+++ src/KMDisclosueButtonView.m
@@ -0,0 +1,31 @@
+//
+//  KMDisclosueButtonView.m
+//  Elements
+//
+//  Created by Kim Hunter on 4/02/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import "KMDisclosueButtonView.h"
+
+@implementation KMDisclosueButtonView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
